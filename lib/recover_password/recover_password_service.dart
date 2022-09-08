9a389f98f1dd40e3d3347dc56edf15f2907dc4f3
sehnryr <youn@melois.dev>
2022-09-08 22:51:10 +0200

    implement send recovery code function

---
+++ lib/recover_password/recover_password_service.dart
@@ -0,0 +1,46 @@
+import 'package:requests/requests.dart';
+
+class RecoverResponseCode {
+  final String _value;
+  const RecoverResponseCode._internal(this._value);
+  @override
+  toString() => 'RecoverResponseCode.$_value';
+
+  static const Error = RecoverResponseCode._internal('Error');
+  static const UsernameError = RecoverResponseCode._internal('UsernameError');
+  static const CodeError = RecoverResponseCode._internal('CodeError');
+  static const Success = RecoverResponseCode._internal('Success');
+}
+
+class RecoverPassword {
+  const RecoverPassword();
+
+  static const String serviceUrl =
+      "https://web.isen-ouest.fr/password/sav/index.php?uri=/demande";
+  static const String proxyUrl = "https://restless-forest-4699.fly.dev/?url=";
+
+  static Future<RecoverResponseCode> sendRecoverRequest({
+    required String username,
+    required int code,
+  }) async {
+    try {
+      var response = await Requests.post("$proxyUrl$serviceUrl", body: {
+        'identifiant': username,
+        'code': code,
+        'submit': null,
+      });
+      response.throwForStatus();
+
+      if (RegExp(r"L'identifiant que vous avez saisi n'est pas valide !")
+          .hasMatch(response.content())) {
+        return RecoverResponseCode.UsernameError;
+      } else if (RegExp(r"Le code que vous avez saisi n'est pas valide !")
+          .hasMatch(response.content())) {
+        return RecoverResponseCode.CodeError;
+      }
+      return RecoverResponseCode.Success;
+    } catch (e) {
+      return RecoverResponseCode.Error;
+    }
+  }
+}
