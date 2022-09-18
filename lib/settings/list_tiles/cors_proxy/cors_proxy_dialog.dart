dd556fb6bac0b586c49e8a8c74742db61a8a513a
sehnryr <youn@melois.dev>
2022-09-18 22:51:02 +0200

    refactor secure_storage import

--- lib/settings/list_tiles/cors_proxy/cors_proxy_dialog.dart
+++ lib/settings/list_tiles/cors_proxy/cors_proxy_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:secure_storage/secure_storage.dart';
 
+import 'package:isen_ouest_companion/secure_storage.dart';
 import 'package:isen_ouest_companion/settings/list_tiles/cors_proxy/cors_proxy_input.dart';
 
 class CORSProxyDialog extends StatefulWidget {
