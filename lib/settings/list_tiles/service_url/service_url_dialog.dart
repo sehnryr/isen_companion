2dfb471436b6464b9021f735b77b39054e16d531
sehnryr <youn@melois.dev>
2022-09-18 21:12:16 +0200

    add service url setting

---
+++ lib/settings/list_tiles/service_url/service_url_dialog.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:secure_storage/secure_storage.dart';
+
+import 'package:isen_ouest_companion/settings/list_tiles/service_url/service_url_input.dart';
+
+class ServiceUrlDialog extends StatefulWidget {
+  final TextEditingController controller;
+  final String title;
+
+  const ServiceUrlDialog({
+    required this.controller,
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ServiceUrlDialogState createState() => ServiceUrlDialogState();
+}
+
+class ServiceUrlDialogState extends State<ServiceUrlDialog> {
+  late String text;
+  late bool error;
+
+  @override
+  void initState() {
+    text = widget.controller.text;
+    error = !_validation(text);
+    super.initState();
+  }
+
+  bool _validation(String text) {
+    return text.isNotEmpty ||
+        RegExp(r'https?:\/\/[^\/]+\.\w{2,}\/?.*\/$').hasMatch(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Text(widget.title),
+      children: [
+        SimpleDialogOption(
+            child: ServiceUrlInput(
+          controller: widget.controller,
+          error: error,
+          onChanged: (String text) =>
+              setState(() => error = !_validation(text.trim())),
+        )),
+        SimpleDialogOption(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              onPressed: () {
+                // reset controller text
+                widget.controller.text = text;
+                Navigator.pop(context, 'Annuler');
+              },
+              child: const Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () {
+                error = !_validation(widget.controller.text);
+                if (!error) {
+                  text = widget.controller.text.trim();
+                  SecureStorage.set(SecureStorageKey.ServiceUrl, text)
+                      .then((value) {
+                    Navigator.pop(context, 'OK');
+                  });
+                }
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ))
+      ],
+    );
+  }
+}
--- lib/settings/settings_constants.dart
+++ lib/settings/settings_constants.dart
@@ -1,4 +1,8 @@
 class DefaultSettings {
+  const DefaultSettings();
+
   static const String defaultProxy =
       "https://restless-forest-4699.fly.dev/?url=";
+  static const String defaultServiceUrl =
+      "https://web.isen-ouest.fr/webAurion/";
 }
