ace784f7fea2a0899bc50f37b8eb976a321b4e7b
sehnryr <youn@melois.dev>
2022-09-19 12:41:51 +0200

    refactor aurion

--- lib/aurion.dart
+++ lib/aurion.dart
@@ -1,14 +1,48 @@
 import 'package:isen_aurion_client/isen_aurion_client.dart';
 
+import 'package:isen_ouest_companion/secure_storage.dart';
+
 class Aurion {
-  const Aurion();
+  static late String _serviceUrl;
 
-  static String _serviceUrl = "";
+  static late IsenAurionClient _client;
 
-  static late IsenAurionClient client;
+  // static Future<List<Map<String, dynamic>>> getSchedule(
+  //     {required String groupId,
+  //     List<Map>? path,
+  //     List<Map>? options,
+  //     DateTime? start,
+  //     DateTime? end}) {
+  //   return client.getSchedule(
+  //     groupId: groupId,
+  //     path: path,
+  //     options: options,
+  //     start: start,
+  //     end: end,
+  //   );
+  // }
 
-  static void init(String serviceUrl) {
+  static Future<List<Map<String, dynamic>>> getUserSchedule(
+      {DateTime? start, DateTime? end}) {
+    return _client.getUserSchedule(
+      start: start,
+      end: end,
+    );
+  }
+
+  static Future<void> login(String username, String password) async {
+    await init(_serviceUrl);
+
+    await SecureStorage.set(SecureStorageKey.Username, username);
+    await SecureStorage.set(SecureStorageKey.Password, password);
+
+    return _client.login(username, password);
+  }
+
+  static Future<void> init(String serviceUrl) async {
+    await SecureStorage.set(SecureStorageKey.ServiceUrl, serviceUrl);
+
     _serviceUrl = serviceUrl;
-    client = IsenAurionClient(serviceUrl: _serviceUrl);
+    _client = IsenAurionClient(serviceUrl: _serviceUrl);
   }
 }
