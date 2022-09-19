953da675fabc469ef92fb52dff4864c88bd1eb6a
sehnryr <youn@melois.dev>
2022-09-19 10:17:27 +0200

    refactor package

--- packages/progress_hud/lib/progress_hud.dart
+++ packages/progress_hud/lib/progress_hud.dart
@@ -1,24 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_progress_hud/flutter_progress_hud.dart' as fph;
+library progress_hud;
 
-class ProgressHUD extends StatelessWidget {
-  final Widget child;
-
-  const ProgressHUD({Key? key, required this.child}) : super(key: key);
-
-  static dynamic of(BuildContext context) {
-    return fph.ProgressHUD.of(context);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return fph.ProgressHUD(
-      backgroundColor: Colors.transparent,
-      borderColor: Colors.transparent,
-      indicatorColor: Theme.of(context).colorScheme.primary,
-      child: Builder(builder: (context) {
-        return child;
-      }),
-    );
-  }
-}
+export 'src/progress_hud.dart';
---
+++ packages/progress_hud/lib/src/progress_hud.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart' as fph;
+
+class ProgressHUD extends StatelessWidget {
+  final Widget child;
+
+  const ProgressHUD({Key? key, required this.child}) : super(key: key);
+
+  static dynamic of(BuildContext context) {
+    return fph.ProgressHUD.of(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return fph.ProgressHUD(
+      backgroundColor: Colors.transparent,
+      borderColor: Colors.transparent,
+      indicatorColor: Theme.of(context).colorScheme.primary,
+      child: Builder(builder: (context) {
+        return child;
+      }),
+    );
+  }
+}
