d90155b3cb43d0f6430a0ea41b54d9027928369e
sehnryr <youn@melois.dev>
2022-10-02 00:59:23 +0200

    make basic events viewer

--- lib/schedule/schedule_page.dart
+++ lib/schedule/schedule_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:isen_ouest_companion/schedule/schedule_week_table.dart';
 
+import 'package:progress_hud/progress_hud.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'package:isen_ouest_companion/aurion.dart';
 import 'package:isen_ouest_companion/schedule/schedule_app_bar.dart';
 import 'package:isen_ouest_companion/schedule/schedule_constants.dart';
+import 'package:isen_ouest_companion/schedule/schedule_week_table.dart';
 
 class SchedulePage extends StatefulWidget {
   const SchedulePage({Key? key}) : super(key: key);
@@ -17,12 +18,13 @@ class SchedulePage extends StatefulWidget {
 class SchedulePageState extends State<SchedulePage>
     with TickerProviderStateMixin {
   late final AnimationController animationController;
-  late List<dynamic> schedule;
+  var schedule = {};
   CalendarFormat calendarFormat = CalendarFormat.week;
   bool isCalendarOpen = false;
   DateTime focusedDay = DateTime.now();
   StartingDayOfWeek startingDayOfWeek = StartingDayOfWeek.sunday;
   late DateTime selectedDay;
+  late final ValueNotifier<List<Event>> selectedEvents;
 
   @override
   void initState() {
@@ -31,6 +33,11 @@ class SchedulePageState extends State<SchedulePage>
       vsync: this,
     );
     selectedDay = focusedDay;
+    selectedEvents = ValueNotifier(getDayEvents(selectedDay));
+    Aurion.getUserSchedule().then((value) {
+      setState((() => schedule = value));
+      selectedEvents.value = getDayEvents(selectedDay);
+    });
     super.initState();
   }
 
@@ -59,6 +66,7 @@ class SchedulePageState extends State<SchedulePage>
         this.selectedDay = selectedDay;
         this.focusedDay = focusedDay;
       });
+      selectedEvents.value = getDayEvents(selectedDay);
     }
   }
 
@@ -66,6 +74,51 @@ class SchedulePageState extends State<SchedulePage>
     setState(() {
       this.focusedDay = focusedDay;
     });
+  }
+
+  List<Event> getDayEvents(DateTime day) {
+    return schedule[day] ?? [];
+  }
+
+  Icon getEventIcon(Event event) {
+    IconData iconData;
+    switch (event.type) {
+      case EventType.course:
+        iconData = Icons.edit_note;
+        break;
+      case EventType.exam:
+        iconData = Icons.school;
+        break;
+      case EventType.meeting:
+        iconData = Icons.groups;
+        break;
+      case EventType.practicalWork:
+        iconData = Icons.engineering;
+        break;
+      case EventType.supervisedWork:
+        iconData = Icons.group;
+        break;
+      default:
+        iconData = Icons.error;
+    }
+    return Icon(iconData);
+  }
+
+  String getEventType(Event event) {
+    switch (event.type) {
+      case EventType.course:
+        return "Cours";
+      case EventType.exam:
+        return "Examen";
+      case EventType.meeting:
+        return "Réunion";
+      case EventType.practicalWork:
+        return "TP";
+      case EventType.supervisedWork:
+        return "TD";
+      default:
+        return "Erreur";
+    }
   }
 
   @override
@@ -89,7 +142,59 @@ class SchedulePageState extends State<SchedulePage>
             calendarFormat: calendarFormat,
             onDaySelected: onDaySelected,
             onPageChanged: onPageChanged,
-          )
+            eventLoader: getDayEvents,
+          ),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: selectedEvents,
+              builder: ((context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    Event event = value[index];
+                    String startHour =
+                        event.start.hour.toString().padLeft(2, '0');
+                    String startMinute =
+                        event.start.minute.toString().padLeft(2, '0');
+                    String endHour = event.end.hour.toString().padLeft(2, '0');
+                    String endMinute =
+                        event.end.minute.toString().padLeft(2, '0');
+                    String time =
+                        "$startHour:$startMinute - $endHour:$endMinute";
+
+                    List<Widget> description = [
+                      Text(time),
+                      Text(event.room),
+                      Text(event.participants.join(', ')),
+                    ];
+
+                    if (event.chapter.isNotEmpty) {
+                      description.insert(1, Text(event.chapter));
+                    }
+
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 6.0,
+                      ),
+                      child: ListTile(
+                        leading: Tooltip(
+                          triggerMode: TooltipTriggerMode.tap,
+                          message: getEventType(event),
+                          child: getEventIcon(event),
+                        ),
+                        title: Text(event.subject),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: description,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
+            ),
+          ),
         ],
       ),
     );
