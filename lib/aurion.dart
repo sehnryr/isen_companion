d6774068c3ef84e6f05d33d7e293f05e913c6dfb
sehnryr <youn@melois.dev>
2022-10-01 23:06:12 +0200

    create event class for schedule

--- lib/aurion.dart
+++ lib/aurion.dart
@@ -2,6 +2,64 @@ import 'package:isen_aurion_client/isen_aurion_client.dart';
 
 import 'package:isen_ouest_companion/secure_storage.dart';
 
+/// Possible event types
+enum EventType {
+  course,
+  exam,
+  leave,
+  meeting,
+  practicalWork,
+  supervisedWork,
+  undefined,
+}
+
+/// Schedule event class.
+class Event {
+  final int id;
+  final EventType type;
+  final DateTime start;
+  final DateTime end;
+  final String room;
+  final String subject;
+  final String chapter;
+  final List<String> participants;
+
+  const Event({
+    required this.id,
+    required this.type,
+    required this.start,
+    required this.end,
+    required this.room,
+    required this.subject,
+    required this.chapter,
+    required this.participants,
+  });
+
+  @override
+  String toString() => subject;
+
+  /// Gets the [EventType] of a [String].
+  static EventType mapType(String rawType) {
+    switch (rawType) {
+      case "CONGES":
+        return EventType.leave;
+      case "COURS":
+        return EventType.course;
+      case "est-epreuve":
+      case "EVALUATION":
+        return EventType.exam;
+      case "REUNION":
+        return EventType.meeting;
+      case "TD":
+        return EventType.supervisedWork;
+      case "TP":
+        return EventType.practicalWork;
+      default:
+        return EventType.undefined;
+    }
+  }
+}
+
 class Aurion {
   static late String _serviceUrl;
 
