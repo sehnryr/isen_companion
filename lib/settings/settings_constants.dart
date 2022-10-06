4e4dd1a507734a888fef802d08ab8ad22dd9beb8
sehnryr <youn@melois.dev>
2022-10-06 14:32:45 +0200

    fix(#33): change proxy hostname

--- lib/settings/settings_constants.dart
+++ lib/settings/settings_constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart';
 class DefaultSettings {
   const DefaultSettings();
 
-  static String get proxyUrl => "https://wild-morning-3373.fly.dev/";
+  static String get proxyUrl => "https://cors-proxy.melois.dev/";
   static String get serviceUrl => "https://web.isen-ouest.fr/webAurion/";
 }
