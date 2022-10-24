55946a356a79aba95003ac456ab5cbf412689258
sehnryr <youn@melois.dev>
2022-10-24 20:56:46 +0200

    feat(recovery): simplify footer

--- lib/recover_password/recover_password_footer.dart
+++ lib/recover_password/recover_password_footer.dart
@@ -5,7 +5,7 @@ import 'package:isen_companion/base/base_footer.dart';
 class RecoverPasswordFooter extends BaseFooter {
   const RecoverPasswordFooter({Key? key})
       : super(
-          "¹: Ce code est inscrit sur votre carte d'étudiant à coté de N° Etudiant, première ligne à gauche sous le code-barres, ou bien sur votre passeport infomatique.",
+          "¹: Ce code est inscrit sur votre carte d'étudiant à coté de N° Etudiant, ou bien sur votre passeport infomatique.",
           key: key,
         );
 }
