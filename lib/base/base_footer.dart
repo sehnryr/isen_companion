823f033593b8d51d071b84f47db1f0007293ee46
sehnryr <youn@melois.dev>
2022-10-24 20:58:03 +0200

    feat(base): add linkable links to footers

--- lib/base/base_footer.dart
+++ lib/base/base_footer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:styled_text/styled_text.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 import 'package:isen_companion/base/base_constants.dart';
 
 class BaseFooter extends StatelessWidget {
@@ -14,11 +17,26 @@ class BaseFooter extends StatelessWidget {
       height: 50,
       padding: padding,
       child: Center(
-          child: Text(
-        text,
-        textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.overline,
-      )),
+        child: StyledText(
+          text: text,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.overline,
+          tags: {
+            'link': StyledTextActionTag(
+              (String? text, Map<String?, String?> attrs) {
+                final String link = attrs['href'] ?? '';
+                if (link.isNotEmpty) {
+                  launchUrl(Uri.parse(link));
+                }
+              },
+              style: const TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          },
+        ),
+      ),
     );
   }
 }
