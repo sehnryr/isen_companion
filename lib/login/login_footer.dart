9abe4ea97d4f390f7cb40571afff1de556a5f099
sehnryr <youn@melois.dev>
2022-10-24 20:58:27 +0200

    feat(login): add privacy policy link to footer

--- lib/login/login_footer.dart
+++ lib/login/login_footer.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:isen_companion/base/base_footer.dart';
 
+const String _url =
+    'https://github.com/sehnryr/isen_companion#politique-de-confidentialité';
+
 class LoginFooter extends BaseFooter {
   const LoginFooter({Key? key})
       : super(
-          "Cette application est à l'usage exclusif des étudiants de l'ISEN.",
+          'Cette application est à l\'usage exclusif des étudiants de l\'ISEN. En utilisant cette application, vous acceptez notre <link href="$_url">Politique de Confidentialité</link>.',
           key: key,
         );
 }
