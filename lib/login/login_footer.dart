e911ec3992be50a97584add77d01d12c1af89402
sehnryr <youn@melois.dev>
2022-10-24 23:41:41 +0200

    fix(footer): use escaped characters for styled_text

--- lib/login/login_footer.dart
+++ lib/login/login_footer.dart
@@ -8,7 +8,7 @@ const String _url =
 class LoginFooter extends BaseFooter {
   const LoginFooter({Key? key})
       : super(
-          'Cette application est à l\'usage exclusif des étudiants de l\'ISEN. En utilisant cette application, vous acceptez notre <link href="$_url">Politique de Confidentialité</link>.',
+          'Cette application est à l&apos;usage exclusif des étudiants de l&apos;ISEN. En utilisant cette application, vous acceptez notre <link href="$_url">Politique de Confidentialité</link>.',
           key: key,
         );
 }
